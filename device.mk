b0954a0fd244e8aadaaecc9dab12ffe308c252cd
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-03-26 01:49:55 +0700

    chime: Enable ADB by default only in non-user builds
    User builds shouldn't have adb enabled by default.
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ia6be823702b47c786f302c67c330c91b973ec68c
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- device.mk
+++ device.mk
@@ -476,6 +476,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
 
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.usb.config=mtp,adb
+endif
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
